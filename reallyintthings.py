d9265363ca20eaf3dba440690ad0554b948d5a8a
TELEUZI <41520794+TELEUZI@users.noreply.github.com>
2019-11-30 18:42:52 +0300

    Add files via upload

--- reallyintthings.py
+++ reallyintthings.py
@@ -1,3 +1,4 @@
+import time
 def make_training_set(training_file_name):
     f=open(training_file_name)
     patient_chars=f.read().split("\n")
@@ -28,6 +29,7 @@ def train_classifier(training_set_list):
         discr_value=(mean_2+mean_4)/2
         clasifier_list.append(discr_value)
         k+=1
+    print(clasifier_list)
     return clasifier_list
 def make_test_set(test_file_name):
     f=open(test_file_name)
@@ -43,12 +45,24 @@ def classify_test_set_list(test_set_list, classifier_list):
     for i in range (0,len(test_set_list)):
         b=0
         k=1
+        chars=["id",
+        "Clump Thickness",
+        "Uniformity of Cell Size",
+        "Uniformity of Cell Shape",
+        "Marginal Adhesion",
+        "Single Epithelial Cell Size",
+        "Bare Nuclei",
+        "Bland Chromatin",
+        "Normal Nucleoli",
+        "Mitoses"]
         test_set_list[i].append('0')
         while k<=9:
             if (int(test_set_list[i][k])<classifier_list[k]):
                 b+=1
                 if b==9:
                     test_set_list[i][11]='2'
+            else: 
+                test_set_list[i].append(chars[k])
             if test_set_list[i][11]!='2':
                 test_set_list[i][11]='4'
             k+=1
@@ -59,7 +73,7 @@ def report_results(result_list):
     for i in range (0,len(result_list)):
         if (result_list[i][10]==result_list[i][11]=='2')|(result_list[i][10]==result_list[i][11]=='4'):           
             accuracy+=1
-            print("Patient number {}: predicted tumor type is {}, real tumor type is the same.".format(result_list[i][0], result_list[i][10] ))
+            print("Patient number {}: predicted tumor type is {}, real tumor type is the same.".format(result_list[i][0], result_list[i][11] ))
         elif ((result_list[i][10]!=result_list[i][11])): 
             print("Patient number {}: predicted tumor type is {}, real tumor type isn't the same.".format(result_list[i][0], result_list[i][11]))
             print("In spite of high", end=" ")
@@ -70,7 +84,7 @@ def report_results(result_list):
                     print(result_list[i][k], end=", ")
     print ("Accuracy equals to {}%.".format(accuracy))
     print ("Reported the results.")
-def main():
+def submain():
     print("Reading in training data...")
     training_file_name="training_data.txt"
     training_set_list=make_training_set(training_file_name)
@@ -87,4 +101,53 @@ def main():
     print("Done classying. \n")
     report_results(result_list)
     print("Program finished. \n")
+def main():
+    while True:
+        ans=input('''Что вы хотите сделать?
+        1)Провести диагностику уже имеющихся пациентов
+        2)Проверить нового пациента онлайн
+        3)Закрыть программу\n''')
+        if ans=="3":
+                print ("Спасибо за использование программы, сделанной Жевгением Чуранковым!")
+                time.sleep(2) #После выведения надписи проходит 2 секунды перед закрытием программы.
+                break #Выходит из главного цикла while
+        elif ans=="1":
+            submain()
+        elif ans=="2":
+            chars=["Clump Thickness",
+            "Uniformity of Cell Size",
+            "Uniformity of Cell Shape",
+            "Marginal Adhesion",
+            "Single Epithelial Cell Size",
+            "Bare Nuclei",
+            "Bland Chromatin",
+            "Normal Nucleoli",
+            "Mitoses"]
+            training_file_name="all_data.txt"
+            training_set_list=make_training_set(training_file_name)
+            print("Done reading training data. \n")
+            print ("Training classifier")
+            classifier_list=train_classifier(training_set_list)
+            k=[]
+            ubnormals=[]
+            b=0
+            for i in range(0,9):
+                k.append(input("Введите {} ".format(chars[i])))
+                if int(k[i])<classifier_list[i+1]:
+                    b+=1
+                else:
+                    ubnormals.append(chars[i])
+            if b==9:
+                print("This patient is fully charged!")
+            else:
+                print("Oh, here we go again.")
+                print("Because of bad", end=" ")
+                for k in range(0,len(ubnormals)):
+                    if k==len(ubnormals)-1:
+                        print(ubnormals[k]+" condition", end="\n")
+                    else:
+                        print(ubnormals[k], end=", ")
+        else:
+            print ("К сожалению, данный пункт меню не найден! Пожалуйста, введите цифры в промежутке от 1 до 4.")
+            continue #Возвращаемся к началу цикла while
 main()
