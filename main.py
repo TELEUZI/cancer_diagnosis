62f72b599ed8946ae610780d5076fa1b1cb46b47
TELEUZI <41520794+TELEUZI@users.noreply.github.com>
2020-07-14 11:38:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import patient
+a = patient.Patient()
+b = patient.Patient()
+num_2 = 0
+num_4 = 0
+with open("training_data.txt") as file:
+    for line in file.read().split("\n"):
+        if "?" in line:
+            continue
+        p = [int(z) for z in line.split(",")]
+        q = patient.Patient(p)
+        if q.patient_class == 2: 
+            a += q
+            num_2 += 1
+        else:
+            b += q
+            num_4 += 1
+classificator = (a/num_2+b/num_4)/2
+l_2 = []
+l_4 = []
+good = 0
+with open("testing_data.txt") as file:
+    for line in file.read().split("\n"):
+        p = [int(z) for z in line.split(",")]
+        q = patient.Patient(p)
+        if q > classificator:
+            l_4.append(q)
+            if q.patient_class == 4:
+                good += 1
+        else:
+            l_2.append(q)
+            if q.patient_class == 2:
+                good += 1
+print(classificator)
+print("ZDOROVIE")
+for i in range(len(l_2)):
+    print(l_2[i].__str__())
+print("BOLNUI")
+for i in range(len(l_4)):
+    print(l_4[i].__str__())
+print (good/(len(l_2)+len(l_4)))
---
+++ patient.py
@@ -0,0 +1,40 @@
+class Patient:
+    def __init__(self, p = [0 for n in range(11)]):
+        self.id = p[0]
+        self.clump_thickness = p[1]
+        self.uniformity_of_cell_size = p[2]
+        self.uniformity_of_cell_shape = p[3]
+        self.marginal_adhension = p[4]
+        self.single_epithelial_cell_size = p[5]
+        self.bare_nuclei = p[6]
+        self.bland_chromatin = p[7]
+        self.normal_nucleoli = p[8]
+        self.mitoses = p[9]
+        self.patient_class = p[10]
+    def __add__(self, other):
+        self.clump_thickness += other.clump_thickness
+        self.uniformity_of_cell_size += other.uniformity_of_cell_size
+        self.uniformity_of_cell_shape += other.uniformity_of_cell_shape
+        self.marginal_adhension += other.marginal_adhension
+        self.single_epithelial_cell_size += other.single_epithelial_cell_size
+        self.bare_nuclei += other.bare_nuclei
+        self.bland_chromatin += other.bland_chromatin
+        self.normal_nucleoli += other.normal_nucleoli
+        self.mitoses += other.mitoses
+        return self
+    def __truediv__(self, other):
+        self.clump_thickness /= other
+        self.uniformity_of_cell_size /= other
+        self.uniformity_of_cell_shape /= other
+        self.marginal_adhension /= other
+        self.single_epithelial_cell_size /= other
+        self.bare_nuclei /= other
+        self.bland_chromatin /= other
+        self.normal_nucleoli /= other
+        self.mitoses /= other
+        return self
+    def __str__(self):
+        return f'{self.id}, {self.clump_thickness}, {self.uniformity_of_cell_size}, {self.uniformity_of_cell_shape}, {self.marginal_adhension}, {self.single_epithelial_cell_size}, {self.bare_nuclei}, {self.bland_chromatin}, {self.normal_nucleoli}, {self.mitoses}, {self.patient_class}'
+    def __gt__(self, other):
+        if ((self.clump_thickness > other.clump_thickness)or (self.uniformity_of_cell_size > other.uniformity_of_cell_size) or (self.uniformity_of_cell_shape > other.uniformity_of_cell_shape) or (self.marginal_adhension > other.marginal_adhension) or (self.single_epithelial_cell_size > other.single_epithelial_cell_size) or (self.bare_nuclei > other.bare_nuclei) or (self.bland_chromatin > other.bland_chromatin) or (self.normal_nucleoli > other.normal_nucleoli) or (self.mitoses > other.mitoses)):
+            return self
